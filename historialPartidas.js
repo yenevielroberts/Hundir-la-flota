ab52be1ce90665da2c60d5cb503ad5277354ad6c
Yeneviel Roberts <yenevielroberts@gmail.com>
2025-05-18 12:23:51 +0200

    mejoras en el historial de partidas

--- historialPartidas.js
+++ historialPartidas.js
@@ -13,30 +13,32 @@ async function todasLasPartidas() {
 
 }
 
-const container = document.getElementById("container");
-let jugadorNombre = prompt("Introduce tu nombre:")
+async function mostrarHistorial() {
+    const container = document.getElementById("container");
+    let jugadorNombre = prompt("Introduce tu nombre:")
 
-if (jugadorNombre != null) {
+    if (jugadorNombre != null) {
 
-    const datos = await todasLasPartidas()
+        const datos = await todasLasPartidas()
 
-    const partidas = datos
-    partidas.forEach(partida => {
-        if (partida.jugador == jugadorNombre) {
-            let div = document.createElement("div");
-            div.innerHTML = `
-                <p>Nombre jugador: ${partida.jugador}</p>
-                <p> partida id: ${partida.id}</p>
-                <p>Estado de la partida: ${partida.estado}</p>
-                <p>Ganador: ${partida.ganador}</p>
+        const partidas = datos
+        partidas.forEach(partida => {
+            if (partida.jugador == jugadorNombre) {
+                let div = document.createElement("div");
+                div.setAttribute("class","infoPartidas")
+                div.innerHTML = `
+                <p><strong>Nombre jugador:</strong> ${partida.jugador}</p>
+                <p><strong> Partida id:</strong> ${partida.id}</p>
+                <p><strong>Estado de la partida:</strong> ${partida.estado}</p>
+                <p><strong>Ganador:</strong> ${partida.ganador}</p>
                 `;
+                container.appendChild(div)
+            }
+        });
 
-            container.appendChild(div)
-            console.log(partida.id)
-        }
-    });
-
+    }
 }
+
 
 document.getElementById("btnCargar").addEventListener("click", () => {
     location.href = "index.html";
@@ -45,3 +47,10 @@ document.getElementById("btnCargar").addEventListener("click", () => {
 document.getElementById("home").addEventListener("click", () => {
     location.href = "index.html";
 })
+
+document.getElementById("nuevoHistorial").addEventListener("click",()=>{
+
+    location.reload()
+})
+
+mostrarHistorial()
